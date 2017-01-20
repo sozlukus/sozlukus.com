cd7015c299a005cb028c2f1ec4fdbcf8ea49c516
Muhammed Ali Sit <masit@outlook.com>
2017-01-20 18:52:03 +0300

    fixed some favit issues, ajax needs to be fixed also

--- sozlukus/favit/managers.py
+++ sozlukus/favit/managers.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
-from django.apps import AppConfig
+from django.apps import apps
 
 
 def _get_content_type_and_obj(obj, model=None):
     if isinstance(model, str):
-        model = AppConfig.get_model(*model.split("."))
+        model = apps.get_model(*model.split("."))
 
     if isinstance(obj, (int, int)):
         obj = model.objects.get(pk=obj)
@@ -41,7 +41,7 @@ class FavoriteManager(models.Manager):
 
         if model:
             if isinstance(model, str):
-                model = AppConfig.get_model(*model.split("."))
+                model = apps.get_model(*model.split("."))
 
             content_type = ContentType.objects.get_for_model(model)
             qs = qs.filter(target_content_type=content_type)
@@ -62,7 +62,7 @@ class FavoriteManager(models.Manager):
 
         # if model is an app_label.model string make it a Model class
         if isinstance(model, str):
-            model = AppConfig.get_model(*model.split("."))
+            model = apps.get_model(*model.split("."))
 
         content_type = ContentType.objects.get_for_model(model)
 
--- sozlukus/sozluk/static/js/favit.js
+++ sozlukus/sozluk/static/js/favit.js
@@ -1,44 +1,44 @@
 $(document).ready(function() {
-    $('.btn.favorite').click(function() {
-        var $obj = $(this);
-        var target_id = $obj.attr('id').split('_')[1];
-        $obj.prop('disabled', true);
-        $.ajax({
-            url: $obj.attr('href'),
-            type: 'POST',
-            data: {target_model: $obj.attr('model'),
-            target_object_id: target_id},
-            success: function(response) {
-                if (response.status == 'added') {
-                    $(".galp-" + target_id).removeClass('fa-heart-o').addClass('fa-heart');
-                }
-                else {
-                    $(".galp-" + target_id).removeClass('fa-heart').addClass('fa-heart-o');
-                }
-                $obj.parent('.favit').children('.ui.pointing').children('.fav-count').text(response.fav_count);
-                $obj.prop('disabled', false);
-            }
-        });
-    });
+  $('.btn.favorite').click(function() {
+      var $obj = $(this);
+      var target_id = $obj.attr('id').split('_')[1];
+      $obj.prop('disabled', true);
+      $.ajax({
+      url: $obj.attr('href'),
+      type: 'POST',
+      data: {target_model: $obj.attr('model'),
+             target_object_id: target_id},
+      success: function(response) {
+          if (response.status == 'added') {
+            $(".favorite-" + target_id).removeClass('fa-heart-o').addClass('fa-heart');
+          }
+          else {
+            $(".favorite-" + target_id).removeClass('fa-heart').addClass('fa-heart-o');
+          }
+          $(".fav-count-" + target_id).load("  .fav-count-" + target_id);
+          $obj.prop('disabled', false);
+      }
+      });
+  });
 
-$('.btn.unfave').click(function() {
-  var $obj = $(this);
-  $obj.prop('disabled', true);
-  $.ajax({
+  $('.btn.unfave').click(function() {
+    var $obj = $(this);
+    $obj.prop('disabled', true);
+    $.ajax({
       url: $obj.attr('href'),
       type: 'POST',
       data: {
-          target_model: $obj.data('model'),
-          target_object_id: $obj.data('id')
+        target_model: $obj.data('model'),
+        target_object_id: $obj.data('id')
       },
       success: function(response) {
-          if (response.status == 'deleted') {
-              $obj.parent().remove();
-          }
+        if (response.status == 'deleted') {
+          $obj.parent().remove();
+        }
       },
       complete: function(response) {
-          $obj.prop('disabled', false);
+        $obj.prop('disabled', false);
       }
+    });
   });
-});
 });
